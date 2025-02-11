cd3288dc7958a16f7f3a1ec62394c02c211ffd43
Laurens de Graaff <38073582+LaurensdeGraaff@users.noreply.github.com>
2025-02-11 15:12:33 +0100

    d1: more testing

--- jaffle_shop_training/analyses/scratchpad.sql
+++ jaffle_shop_training/analyses/scratchpad.sql
@@ -1,5 +1,6 @@
 {# select * from dbt_lgraaff.dim_customers;
  #}
 
- select * from {{ref('fct_orders')}}
- where payment_status NOT IN ('returned','completed','shipped','return_pending','placed')
+ select order_status from {{ref('stg_jaffle_shop_orders')}}
+ where order_status NOT IN ('returned','comnpleted','return_pending','shipped','placed')
+ group by order_status
